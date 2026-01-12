dce830d080044089fb1c821ef4fac09e257f1365
fronik <askolya23@gmail.com>
2026-01-12 23:01:30 +0200

    [*] - refactor interface

--- types/car.types.ts
+++ types/car.types.ts
@@ -5,9 +5,12 @@ export interface ICar {
   price_per_day: string;
   rating: number;
   image?: string;
-  tank_capacity?: string;
-  max_speed?: string;
-  fuel_type?: string;
+  tank_capacity: string;
+  max_speed: string;
+  fuel_type: string;
   created_at?: string;
   updated_at?: string;
+  engine_type: string;
+  wheel_drive: string;
+  car_seats: string;
 }
